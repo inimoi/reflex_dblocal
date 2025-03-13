2b5881a16c496ec099093e7442a1aa0a9041c597
inimoi <inimonizan@gmail.com>
2025-03-13 08:10:16 +0100

    la defi dos

--- rxconfig.py
+++ rxconfig.py
@@ -9,6 +9,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 config = rx.Config(
     app_name="reflex_dblocal_venv",
     db_url=DATABASE_URL,
-    api_url="https://reflexdblocal-production.up.railway.app/",
+    api_url="https://reflexdblocal-production.up.railway.app",
     #cors_allowed_origins =["https://reflex-dblocal.vercel.app/"]
 )
