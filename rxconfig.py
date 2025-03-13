4983ea9432eb76016fb8d55c3c0d776ec527a954
inimoi <inimonizan@gmail.com>
2025-03-13 09:12:24 +0100

    la defi cuatro

--- rxconfig.py
+++ rxconfig.py
@@ -10,5 +10,5 @@ config = rx.Config(
     app_name="reflex_dblocal_venv",
     db_url=DATABASE_URL,
     api_url="https://reflexdblocal-production.up.railway.app",
-    #cors_allowed_origins =["https://reflex-dblocal.vercel.app/"]
+    cors_allowed_origins =["https://reflex-dblocal.vercel.app"]
 )
